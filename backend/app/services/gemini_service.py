01e9fc55358744b5c1cf435c09f6fd000479ae12
pichkaribalamm <dhawalrajeshjain@gmail.com>
2026-08-09 15:41:50 +0530

    Update gemini_service.py

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -1,1 +1,32 @@
+import os
 
+from google import genai
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class GeminiService:
+
+    def __init__(self):
+        api_key = os.getenv("GEMINI_API_KEY")
+
+        if not api_key:
+            raise ValueError(
+                "GEMINI_API_KEY is not configured."
+            )
+
+        self.client = genai.Client(api_key=api_key)
+
+    def generate(self, prompt: str, response_schema):
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": response_schema.model_json_schema(),
+            },
+        )
+
+        return response.text
